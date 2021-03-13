9f479228d0f606c2a6d97d0265bb9815f7bab973
bayu2septian <boye.septian@gmail.com>
2021-03-13 16:32:35 +0700

    bikin home web

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['login']]);
+    }
+    public function login()
+    {
+        return view('login');
+    }
+    public function do_login(Request $request)
+    {
+        $user = $request->input('username');
+        $pass = $request->input('password');
+        $au = User::where([['login', $user], ['pass', $pass]])->first();
+        // dd($au);
+        if ($au) {
+            $token = auth('api')->login($au);
+            $this->status = true;
+            $this->message = 'ok';
+            $this->data = ['lokasi' => $au->lokasi, 'kode' => $au->kpusk, 'nama' => $au->nama, 'level' => $au->level, 'access_token' => $token, 'token_type' => 'bearer', 'expires_in' => 3600];
+        }
+        return response()->json([
+            'status'    => $this->status,
+            'message'   => $this->message,
+            'data'      => $this->data,
+        ]);
+    }
 }
